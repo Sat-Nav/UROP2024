64c6efa693de2b0a705a2527838e6017efda2e54
Aarnav Jain <aarnavjain.main@gmail.com>
2024-07-11 02:57:00 +0100

    identified interesting period in march 2019 and slightly generalised the CDAWeb function (needs to be adjusted so it can accept the final format of CDA .cdf file)

--- src/import_cdaweb.py
+++ src/import_cdaweb.py
@@ -1,17 +1,20 @@
 from spacepy import pycdf
 import pandas as pd
 
-def read_CDAWeb(mag_file_path, swepam_file_path):
-    mag = pycdf.CDF(mag_file_path)
-    swepam = pycdf.CDF(swepam_file_path)
+def read_CDAWeb(*files, mag_file_path=None):
+    if mag_file_path:
+        mag = pycdf.CDF(mag_file_path)
 
-    d = {"Epoch" : mag["Epoch"][...]}
-    for i, coord in enumerate(mag["metavar0"][...]):
-        d[coord] = mag["BGSM"][...][:,i]
-    mag_df = pd.DataFrame.from_dict(d)
+        d = {"Epoch" : mag["Epoch"][...]}
+        for i, coord in enumerate(mag["metavar0"][...]):
+            d[coord] = mag["BGSM"][...][:,i]
+        mag_df = pd.DataFrame.from_dict(d)
 
-    d = {}
-    for i in swepam:
-        d[i] = swepam[i][...]
-    swepam_df = pd.DataFrame.from_dict(d)
-    return mag_df, swepam_df
+    dfs = []
+    for file in files:
+        cdf = pycdf.CDF(file)
+        d = {}
+        for i in cdf:
+            d[i] = cdf[i][...]
+        dfs.append(pd.DataFrame.from_dict(d))
+    return mag_df, *dfs
